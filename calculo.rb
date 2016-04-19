11e6f03207bd3a659f02977f848a80da8576cc14
toblerone554 <toblerone554@gmail.com>
2016-04-19 19:04:30 +0200

    Uso de puts con Ruby

---
+++ calculo.rb
@@ -0,0 +1,15 @@
+# ¿Cuantos horas tiene un año?
+
+puts 24 * 365 
+
+# ¿Cuantos minutos tiene una decada?
+
+puts ((60*24)*365)*10
+
+# ¿Cuantos segundos de edad tienes?
+
+puts (((60*60)*24)*365) * 30
+
+# ¿Cuantos minutos tiene un año bisiesto?
+
+puts (60 * 24) * 366
